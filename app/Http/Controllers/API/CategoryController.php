9882ec2fee8a193ab4b8195d88bf2138eb77ac63
akashcseem <akashcseuu026@gmail.com>
2019-09-14 11:56:17 +0600

    procfile create

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -5,34 +5,29 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Category;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 use DB;
 
 class CategoryController extends Controller
 {
   public function index()
   {
-      // load page with initial data
-      return Category::latest()->paginate(5);
+    return Category::latest()->paginate(5);
   }
+
   public function category_list()
   {
-      // load page with initial data
-      return DB::table('categories')->paginate(255);
+    return DB::table('categories')->paginate(255);
   }
 
   public function store(Request $request)
   {
-    // dd($request->all());
-    // validation
     $this->validate($request, [
-      'name'      => 'required|string|max:255|min:2',
+      'name'         => 'required|string|max:255|min:2',
       'description'  => 'required|string|min:4|max:191',
     ]);
     // insert data
     return Category::create([
-      'name'      => $request['name'],
+      'name'         => $request['name'],
       'description'  => $request['description'],
     ]);
   }
@@ -40,31 +35,23 @@ class CategoryController extends Controller
 
   public function show($id)
   {
-      // display single item detail
+    // display single item detail
   }
 
-  public function update(Request $request, $id)
+  public function update(Request $request, Category $category)
   {
-      // update data
-      $category = Category::findOrFAil($id);
-      $this->validate($request, [
-        'name'      => 'required|string|max:255|min:2',
-        'description'  => 'required|string|min:4|max:191',s
-      ]);
-      $category->update($request->all());
-      return ['message'=>'Getting id is: ' . $id];
+    $this->validate($request, [
+      'name'         => 'required|string|max:255|min:2',
+      'description'  => 'required|string|min:4|max:191',
+    ]);
+
+    $category->update($request->all());
+    return ['message' => 'Category updated successfull'];
   }
 
-  public function destroy($id)
+  public function destroy(Category $category)
   {
-      //  delete
-      // select id
-      $category = Category::findOrFail($id);
-      // delete user
-      $category->delete();
-      // redirect back
-      return ['message' => 'Category deleted successfully'];
+    $category->delete();
+    return ['message' => 'Category deleted successfully'];
   }
-
-
 }
--- resources/views/public_user/inc/sidebar-category.blade.php
+++ resources/views/public_user/inc/sidebar-category.blade.php
@@ -1,13 +1,6 @@
 <div class="mr-2 ml-5 mb-3" style="border : 1px solid gray; height: 100%; margin-right: -5px;">
 
 
-  {{-- <div class="custom-control custom-checkbox mb-2"> --}}
-    {{-- <input type="checkbox"checked class="custom-control-input" id="111">
-    <a href="{{ route('productByCategory', 1) }}">
-    <label class="custom-control-label font-weight-normal pt-1" style="font-size: 14px; cursor: pointer;" for="111"> All Category</label>
-    </a> --}}
-
-  {{-- </div> --}}
 
   <div class="category-sidebar">
     @if ($selectedCategory == 0)
@@ -32,11 +25,6 @@
           {{ $category->name }}
         </a>
       </h6>
-        {{-- <div class="custom-control custom-checkbox mb-2">
-          <input type="checkbox" class="custom-control-input" id="{{ $category->id }}">
-          <label class="custom-control-label font-weight-normal pt-1" style="font-size: 14px; cursor: pointer;" for="{{ $category->id }}"> {{ $category->name }} </label>
-        </div> --}}
-
     @endforeach
 
   </div>
